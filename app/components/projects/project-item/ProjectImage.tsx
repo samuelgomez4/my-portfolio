f6672fa5d78d49ac41e400ef30c971b759e14788
Samuel Gomez <samuelgomez4010@gmail.com>
2025-02-24 12:17:37 -0500

    Refactor ProjectItem and ProjectImage components for improved layout and responsiveness; adjust image positioning and sizing

--- app/components/projects/project-item/ProjectImage.tsx
+++ app/components/projects/project-item/ProjectImage.tsx
@@ -8,12 +8,12 @@ export function ProjectImage({ name, images, className }: Props) {
   return (
     <div className={`relative overflow-hidden z-0  ${className}`}>
       <img
-        className="absolute bottom-3 -left-6 rounded-xl w-60 group-hover:-translate-x-5 group-hover:translate-y-2 transition-all duration-300 shadow-[8px_2px_12px_-5px_black]"
+        className="absolute bottom-[5%] -left-[6%] rounded-xl w-3/5 group-hover:-translate-x-[8%] group-hover:translate-y-[5%] transition-all duration-300 shadow-[8px_2px_12px_-5px_black] group-hover:shadow-none"
         src={image1}
         alt={`An image that shows a demo of the project ${name}`}
       />
       <img
-        className="absolute -z-10 rounded-xl w-60 -right-6 top-3 group-hover:translate-x-5 group-hover:-translate-y-2 transition-all duration-300 shadow-[8px_4px_12px_-5px_black]"
+        className="absolute -z-10 rounded-xl w-3/5 -right-[6%] top-[5%] group-hover:translate-x-[8%] group-hover:-translate-y-[5%] transition-all duration-300 shadow-[8px_4px_12px_-5px_black] group-hover:shadow-none"
         src={image2}
         alt={`An image that shows a demo of the project ${name}`}
       />
--- app/components/projects/project-item/ProjectItem.tsx
+++ app/components/projects/project-item/ProjectItem.tsx
@@ -8,16 +8,16 @@ interface Props {
 
 export function ProjectItem({ project }: Props) {
   return (
-    <article className="relative group flex rounded-xl overflow-hidden  shadow-xl">
-      <div className="relative overflow-hidden">
+    <article className="relative group flex flex-col sm:flex-row rounded-xl overflow-hidden  shadow-xl sm:items-center gap-8">
+      <div className="relative overflow-hidden  flex-[2]">
         <div className="rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-purple-500 w-2/3 aspect-video -z-10 blur-3xl" />
         <ProjectImage
           name={project.name}
           images={project.images}
-          className="w-[400px] h-60 bg-white/30 rounded-xl"
+          className="w-full aspect-video bg-white/30 rounded-xl"
         />
       </div>
-      <div className="flex-1 px-8 flex flex-col justify-center">
+      <div className="flex-[3] flex flex-col justify-center">
         <h3 className="text-2xl font-semibold text-white mb-2">{project.name}</h3>
         <p className="text-lg text-gray-400 mb-3 text-pretty">{project.description}</p>
         <div className="flex gap-4 mb-6">
